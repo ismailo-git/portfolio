935f99fc18960eb55c1ff832a5bfc3087ff61a99
Ismail Hassan <ihassan.ahmedev@gmail.com>
2022-06-26 18:26:49 +0200

    Loader added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 const swup = new Swup();
+
+function loader() {
+	document.querySelector(".loader").classList.add("fade-out");
+}
+
+function fadeOut() {
+	setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
